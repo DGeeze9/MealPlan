31d13ac71dfece0c6410a8bd874236d9071b09ef
Danny Garcia <dannygarcia79@gmail.com>
2017-02-15 22:08:21 +0000

    created a document ready function

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("connected");
+$(document).ready(()=>{
+    $('#tables').toggleClass("hidden");
+});
+
+// Tips button even listerner
+
+// shopping list event listener
+
+// serving sizes event listener
+
+// diet plan event listener
