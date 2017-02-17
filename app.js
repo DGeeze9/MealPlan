781df9116cd1cbd53a93a3e662001da7a1ba7812
Danny Garcia <Danny@danny-garcias-imac.local>
2017-02-17 02:37:10 -0600

    added jQuery funtionality to nav buttons

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 $(document).ready(()=>{
-    $('#tables').toggleClass("hidden");
+    $('#tips').hide();
+    $('#grocerylist').hide();
+    $('#servingsizes').hide();
+    $('#tables').show();
 });
 
-// Tips button even listerner
-
-// shopping list event listener
-
-// serving sizes event listener
-
-// diet plan event listener
+// Nav Tags event handler
+$('nav').on('click', (event)=>{
+	
+	if (event.target.textContent == 'Diet Plan'){
+		$('#tips').hide();
+	    $('#grocerylist').hide();
+	    $('#servingsizes').hide();
+	    $('#tables').show();
+	} else if (event.target.textContent == 'Tips'){
+		$('#tables').hide();
+	    $('#grocerylist').hide();
+	    $('#servingsizes').hide();
+	    $('#tips').show();
+	} else if (event.target.textContent == 'Shopping List') {
+		$('#tips').hide();
+	    $('#tables').hide();
+	    $('#servingsizes').hide();
+	    $('#grocerylist').show();
+	} else if (event.target.textContent == 'Serving Sizes') {
+		$('#tips').hide();
+	    $('#grocerylist').hide();
+	    $('#tables').hide();
+	    $('#servingsizes').show();
+	}
+});
